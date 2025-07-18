9492b83308f862c3245cf623b2e78b3e9a7724e7
AsherI <uixprt@gmail.com>
2025-07-18 07:14:55 +0300

    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-
-const compat = new FlatCompat({});
-
 module.exports = {
-  extends: compat.extends('next/core-web-vitals', 'next/typescript'),
+  extends: [
+    'next/core-web-vitals',
+    'next',
+    'next/typescript'
+  ],
   rules: {
     '@next/next/no-page-custom-font': 'off',
   },
